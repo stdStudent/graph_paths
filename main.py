07c48ee9e15d43b7edd5b6ed2d550dbff16e503d
stdStudent <78802930+stdStudent@users.noreply.github.com>
2022-11-16 17:00:31 +0500

    lab1: added timer.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
 import copy
 from pathlib import Path
+import time
 
 
 class Graph:
@@ -57,8 +58,9 @@ class Graph:
         ver = int(input("Enter the first vertex's index:\n"))
         if ver < 0 or ver > self.verteces - 1:
             exit("Wrong vertex index.")
-        distances[ver] = 0
 
+        begin = time.time()
+        distances[ver] = 0
         min_idx = 0
         while min_idx < math.inf:
             min_idx = math.inf
@@ -76,35 +78,44 @@ class Graph:
                             distances[i] = temp
                 already_proceeded_verteces[min_idx] = 0
 
+        end = time.time()
+        print(f"Total runtime of the function is {end - begin} seconds.")
+
         self.write_result(distances)
 
         del tmp_graph
 
     def run_ford_bellman(self):
-        tmp_matrix = copy.deepcopy(self.graph)
+        tmp_graph = copy.deepcopy(self.graph)
         distances = [math.inf] * self.verteces
 
         ver = int(input("Enter the first vertex's index:\n"))
         if ver < 0 or ver > self.verteces - 1:
             exit("Wrong vertex index.")
+
+        begin = time.time()
         distances[ver] = 0
+        for k in range(0, self.verteces - 1):
+            for i in range(0, self.verteces):
+                for j in range(0, self.verteces):
+                    if distances[j] > distances[i] + tmp_graph[i][j] and distances[i] != math.inf:
+                        distances[j] = distances[i] + tmp_graph[i][j]
 
         for k in range(0, self.verteces - 1):
             for i in range(0, self.verteces):
                 for j in range(0, self.verteces):
-                    if distances[j] > distances[i] + tmp_matrix[i][j] and distances[i] != math.inf:
-                        distances[j] = distances[i] + tmp_matrix[i][j]
-
-        for k in range(0, self.verteces - 1):
-            for i in range(0, self.verteces):
-                for j in range(0, self.verteces):
-                    if distances[j] > distances[i] + tmp_matrix[i][j] and distances[i] != math.inf:
+                    if distances[j] > distances[i] + tmp_graph[i][j] and distances[i] != math.inf:
                         exit("Found negative weight cycle in the given graph.")
+
+        end = time.time()
+        print(f"Total runtime of the function is {end - begin} seconds.")
 
         self.write_result(distances)
 
     def run_floyd_warshall(self):
         tmp_graph = copy.deepcopy(self.graph)
+
+        begin = time.time()
 
         for k in range(0, self.verteces):
             for i in range(0, self.verteces):
@@ -114,14 +125,17 @@ class Graph:
                     if tmp_graph[i][k] < math.inf and tmp_graph[k][j] < math.inf:
                         tmp_graph[i][j] = min(tmp_graph[i][j], tmp_graph[i][k] + tmp_graph[k][j])
 
+        end = time.time()
+        print(f"Total runtime of the function is {end - begin} seconds.")
+
         self.write_result_matrix(tmp_graph)
 
         del tmp_graph
 
 
 def main():
-    g = Graph("test3.txt")
-    g.run_floyd_warshall()
+    g = Graph("test5.txt")
+    g.run_dijkstra()
 
 
 if __name__ == "__main__":
